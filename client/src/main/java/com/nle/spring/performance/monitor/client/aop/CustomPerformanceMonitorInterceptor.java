6e4065b57ab0964b4dfb7a364308ad990083f24d
nhungle <misssg85@gmail.com>
2022-10-24 17:23:43 -0500

    AOP perfomrance monitor

--- client/src/main/java/com/nle/spring/performance/monitor/client/aop/CustomPerformanceMonitorInterceptor.java
+++ client/src/main/java/com/nle/spring/performance/monitor/client/aop/CustomPerformanceMonitorInterceptor.java
@@ -4,16 +4,21 @@ import org.aopalliance.intercept.MethodInvocation;
 import org.apache.commons.logging.Log;
 import org.springframework.aop.interceptor.AbstractMonitoringInterceptor;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class CustomPerformanceMonitorInterceptor extends AbstractMonitoringInterceptor {
 	private static long warnCount = 0;
     private static long warnThreshold = 10;
 
     public CustomPerformanceMonitorInterceptor(boolean useDynamicLogger) {
+    	log.info("*** CustomPerformanceMonitorInterceptor init");
         setUseDynamicLogger(useDynamicLogger);
     }
 
     @Override
-    protected Object invokeUnderTrace(MethodInvocation invocation, Log log) throws Throwable {
+    protected Object invokeUnderTrace(MethodInvocation invocation, Log logg) throws Throwable {
+    	log.info("*** CustomPerformanceMonitorInterceptor invokeUnderTrace");
         String name = createInvocationTraceName(invocation);
         long start = System.currentTimeMillis();
         log.info("     Method " + name);
--- client/src/main/java/com/nle/spring/performance/monitor/client/aop/PerformanceMonitorConfiguration.java
+++ client/src/main/java/com/nle/spring/performance/monitor/client/aop/PerformanceMonitorConfiguration.java
@@ -10,32 +10,46 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @EnableAspectJAutoProxy
 @Aspect
+@Slf4j
 public class PerformanceMonitorConfiguration {
 	/* Please view lesson 02_04 for a detailed explanation of the below code */
 
+	@Bean
     public PerformanceMonitorInterceptor invocationInterceptor() {
+		log.info("*** invocationInterceptor init");
         return new PerformanceMonitorInterceptor(true);
     }
 
-    @Pointcut("execution(public String com.nle.spring.performance.monitor.client.controller.DemoController.*(..))")
-    public void monitor() { }
+    @Pointcut("execution(public * com.nle.spring.performance.monitor.client.controller.DemoController.*(..))")
+    public void monitor() { 
+    	log.info("*** PerformanceMonitorConfiguration monitor");
+    }
 
+    @Bean
     public Advisor performanceMonitorAdvisor() {
         AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
         pointcut.setExpression("com.nle.spring.performance.monitor.client.aop.PerformanceMonitorConfiguration.monitor()");
         return new DefaultPointcutAdvisor(pointcut, invocationInterceptor());
     }
 
+    @Bean
     public CustomPerformanceMonitorInterceptor customInterceptor() {
+    	log.info("*** CustomPerformanceMonitorInterceptor init");
+    	
         return new CustomPerformanceMonitorInterceptor(true);
     }
 
-    @Pointcut("execution(public String com.nle.spring.performance.monitor.client.domain.CpuLoader.expensiveCalculation(..))")
-    public void customMonitor() { }
+    @Pointcut("execution(public * com.nle.spring.performance.monitor.client.domain.CpuLoader.expensiveCalculation(..))")
+    public void customMonitor() {
+    	log.info("*** bind to expensiveCalculation");
+    }
 
+    @Bean
     public Advisor customPerformanceMonitorAdvisor() {
         AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
         pointcut.setExpression("com.nle.spring.performance.monitor.client.aop.PerformanceMonitorConfiguration.customMonitor()");

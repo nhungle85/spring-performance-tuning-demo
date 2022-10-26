35ccf1e5b66e1676040292f523d4222a270bcb47
nhungle <misssg85@gmail.com>
2022-10-26 00:05:33 -0500

    Show application start up events/time + hibernate log

--- demo-performance-api/src/main/java/com/nle/spring/performance/monitor/api/DemoPerformanceApiApplication.java
+++ demo-performance-api/src/main/java/com/nle/spring/performance/monitor/api/DemoPerformanceApiApplication.java
@@ -2,12 +2,19 @@ package com.nle.spring.performance.monitor.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class DemoPerformanceApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoPerformanceApiApplication.class, args);
+		//SpringApplication.run(DemoPerformanceApiApplication.class, args);
+		SpringApplication apiDemoApplication = new SpringApplication(DemoPerformanceApiApplication.class);
+		BufferingApplicationStartup bas = new BufferingApplicationStartup(1000); //set to 1000 events are allowed -- usually just 400 events
+		//bas.addFilter(startupStep -> startupStep.getName().startsWith("spring.boot.application.starting"));
+		apiDemoApplication.setApplicationStartup(bas);
+		
+		apiDemoApplication.run(args);
 	}
 
 }
